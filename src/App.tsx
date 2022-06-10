a6f59a9aba30de1ce706690bcacb9c5959023df8
Steven <steven.pilagan@habitap.app>
2022-06-10 17:28:58 +0800

    master - added cookie notification box

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css'
 import AppNavbar from './layout/AppNavbar'
 import AppMain from './layout/AppMain'
 import AppFooter from './layout/AppFooter'
+import CookieNotification from './components/CookieNotification'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <AppNavbar />
       <AppMain />
       <AppFooter />
+      <CookieNotification />
     </div>
   )
 }
---
+++ src/components/CookieNotification.tsx
@@ -0,0 +1,17 @@
+const CookieNotification = () => {
+  return (
+    <div className='cookie-notification'>
+      <div className="container">
+        <div className="message">
+          <i className='fa-solid fa-cookie' />
+          <p>Fugiat sunt aute ullamco quis do sit irure sunt ut ullamco in consectetur laborum reprehenderit. Proident labore duis consequat fugiat elit elit laborum sunt. Magna aliquip dolore incididunt officia nisi exercitation adipisicing deserunt voluptate labore in excepteur. Fugiat officia qui ullamco magna duis aute fugiat deserunt eiusmod nostrud. Adipisicing fugiat cillum minim proident.</p>
+        </div>
+        <div>
+          <button>Got it</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CookieNotification
